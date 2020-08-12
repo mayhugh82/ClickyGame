43cbe6f27753aa28f71e884d5a1a621187e4c2a7
Diana Mayhugh <dianamayhugh@gmail.com>
2020-08-12 17:42:49 -0400

    app.js logic

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import FriendCard from "./components/Card";
+import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import Score from "./components/Score";
+import ppg from "./friends.json";
+import "./App.css";
 
 class App extends Component {
   // Setting this.state.ppg to the cards json array
@@ -15,7 +17,7 @@ class App extends Component {
     status: "",
   };
 
-  //shuffle the ppg cards in the browser when clicked
+  //shuffle the pup cards in the browser when clicked
   shuffleScoreCard = (id) => {
     let clickedPPGIds = this.state.clickedPPGIds;
 
@@ -32,7 +34,7 @@ class App extends Component {
       if (clickedPPGIds.length === 8) {
         this.setState({
           score: 8,
-          status: "You Won! Great Job! Click to play again!",
+          status: "You Won! Great Job, Smartie! Click to play again!",
           clickedPPGIds: [],
         });
         console.log("You Win");
@@ -53,22 +55,22 @@ class App extends Component {
     }
   };
 
-  // Map over this.state.friends and render a Card component for each card object
+  // Map over this.state.cards and render a Card component for each card object
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Clicky Game</h1>
+          <h1 className="App-title">The Clickster</h1>
           <p className="App-intro">Try not to click the same image twice!</p>
         </header>
         <Score total={this.state.score} goal={8} status={this.state.status} />
         <Wrapper>
-          {this.state.ppg.map((friends) => (
+          {this.state.ppg.map((puppy) => (
             <Card
               shuffleScoreCard={this.shuffleScoreCard}
-              id={friends.id}
-              key={friends.id}
-              image={friends.image}
+              id={puppy.id}
+              key={puppy.id}
+              image={puppy.image}
             />
           ))}
         </Wrapper>
